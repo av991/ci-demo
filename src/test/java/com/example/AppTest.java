4b832c34c83ca66b2b7127d1f1bd5c88ccb98b19
av991 <162190126+av991@users.noreply.github.com>
2025-07-07 12:51:53 -0500

    Fixed build failiure with surefire plugin

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest {
     @Test
     public void testAdd() {
         App app = new App();
-        int result = app.add(2, 3);
+        int result = app.return_sum(2, 3);
         assertEquals(5, result);
     }
 }
